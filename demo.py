9aca1353ecca04b99e098d669f9efbde393b89b0
hwp <1987963805@qq.com>
2020-06-09 15:41:06 +0800

    commit

---
+++ demo.py
@@ -0,0 +1,22 @@
+print("xxx".join("123"))
+print("hello".join("python"))
+
+print("-".join("hello"))
+
+
+print("-".join(["1", "23", "45"]))
+
+print("-".join({"name": "xiaoming", "age": "18"}))
+
+
+print("2020-12-20".replace("-", "*"))
+
+a = "2020-12-20".split("-")
+print(a)
+print("*".join(a))
+
+a = ['2020', '12', '20']
+print("".join(a))
+
+a = ['小明', '20200202', 'xxxxx']
+
